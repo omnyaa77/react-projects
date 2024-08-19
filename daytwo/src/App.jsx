11ef32e85cc797caaafeb72ee5153c3396cf5901
omnyaa77 <omnyaali96@gmail.com>
2024-08-19 18:33:41 +0300

    Delete daytwo directory

--- daytwo/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import FirstComponent from './components/FirstComponent/FirstComponent'
-
-function App() {
-  return (
-<FirstComponent> </FirstComponent>  ) 
-}
-
-export default App
--- daytwo/src/components/FirstComponent/FirstComponent.jsx
+++
@@ -1,8 +0,0 @@
-function FirstComponent () {
-return ( 
-    <div> 
-        <h2> my first component  tota </h2>
-    </div>
-)
-}
-export default FirstComponent
